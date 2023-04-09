6fd05bddfb15b0aedda6ba983cfec8904fc29b21
0renu <orenams01@gmail.com>
2023-04-09 12:55:18 +0200

    SAD - Composition ( revoir homogenous map )

--- sujet_4/sujet4.cpp
+++ sujet_4/sujet4.cpp
@@ -11,13 +11,26 @@ int main(int argc, char* argv[])
     std::vector<OCTET> image_gauche = lire_image_pgm("../Images_RGB/im2.pgm");
     std::vector<OCTET> image_droite = lire_image_pgm("../Images_RGB/im6.pgm");
     
-    //std::cout<< _nH << " X " << _nW <<std::endl;
+    std::vector<OCTET> Disparity = compute_disparity_map(image_droite , image_gauche , 4 , 150);
+    
+    ecrire_image_pgm(Disparity,"DisparityMap.pgm");
+    
+    fermetureedge(image_droite , image_gauche , 30);
    
+    std::vector<OCTET> ImgEdgeDistd = edge_distance(image_droite , image_gauche );
+    std::vector<OCTET> ImgEdgeDistg = edge_distance(image_gauche, image_droite);
     
     
-    //std::vector<OCTET> ImgOut = disparatemap(image_gauche , image_droite ,3, 5 , 0.9);
-    std::vector<OCTET> ImgEdgeDist = edge_distance(image_gauche ,image_droite , 25);
-    ecrire_image_pgm(ImgEdgeDist,"IMGEDGEDist.pgm");
+    ecrire_image_pgm(image_droite,"edgedfermeture.pgm");
+    ecrire_image_pgm(image_gauche,"edgegfermeture.pgm");
+    
+    ecrire_image_pgm(ImgEdgeDistd,"test2.pgm");
+    ecrire_image_pgm(ImgEdgeDistg,"test1.pgm");
+    
+    std::vector<OCTET> homogenous = homogenparam(image_gauche, 1, 100);
+    
+    ecrire_image_pgm(FinalDisparity(Disparity,ImgEdgeDistg,homogenous, 150),"finaldisparity.pgm");
+    
     //image_gauche = image4regionsrecursive(image_gauche , _nH , _nW , 1500);
     //image_droite = image4regionsrecursive(image_gauche , _nH , _nW , 1000);
 
